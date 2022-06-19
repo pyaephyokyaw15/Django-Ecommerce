997156380f985169bac5971cae3ce0a8a2b9f606
pyaephyokyaw15 <pyaekyaw1571999@gmail.com>
2022-06-19 11:31:19 +0630

    Product, Category Models

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'store.apps.StoreConfig',
+    'categories.apps.CategoriesConfig'
 ]
 
 MIDDLEWARE = [
@@ -120,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+# Media fiels
+MEDIA_ROOT = BASE_DIR /'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ store/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.urls import reverse
+from categories.models import Category
+
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=255, blank=True)
+    price = models.IntegerField()
+    image = models.ImageField()
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    categories = models.ManyToManyField(Category)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
+
+    def __str__(self):
+        return self.name
