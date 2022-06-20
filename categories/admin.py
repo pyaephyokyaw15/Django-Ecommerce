51b006b78517f194365a506695d18d9f3ff5e41f
pyaephyokyaw15 <pyaekyaw1571999@gmail.com>
2022-06-20 19:39:41 +0630

    Prepropulated Fileds on slug

--- categories/admin.py
+++ categories/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Category
 
+
 # Register your models here.
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Product
 
+
 # Register your models here.
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Product, ProductAdmin)
